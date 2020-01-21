9aac93affbd901ebba6304760136c4eb2f37c9ad
sergey <skazantsev@lineate.com>
2020-01-21 19:20:41 +0600

    typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min (array = []) {
+  return array.length === 0 ? 0 : Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (array = []) {
+  return array.length === 0 ? 0 : Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (array = []) {
+  return array.length === 0 ? 0 : array.reduce((a,b) => a + b, 0) / array.length;
+};
